f80015782e10044a60e88d7b0af361bb88c61cec
jeliaaa <julia.aleqsandre@gmail.com>
2023-08-21 00:51:56 +0400

    filtering vacancies

--- src/ReusableComponents/navbox/NavBox.jsx
+++ src/ReusableComponents/navbox/NavBox.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './navbox.scss'
-const NavBox = ({name, to}) => {
+const NavBox = ({ name, to }) => {
     return (
         <div className="nav-box">
             <Link to={to}><h3>{name}</h3></Link>
+            <h3>{name}</h3>
         </div>
     )
 }
 
-export default NavBox
+export default React.memo(NavBox)
